3933f273d30c526bd2ecbdd108244023f2d8a3de
guischroeder <guilherme.schroederr@gmail.com>
2023-11-16 16:51:42 -0300

    update config

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     showtabline = 0,
+    cmdheight = 1,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
